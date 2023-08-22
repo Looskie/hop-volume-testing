f3c635d3541519750c06f3427463b864a5caac90
Cody Miller <hello@looskie.com>
2023-08-22 14:33:56 -0400

    .

--- main.go
+++ main.go
@@ -6,14 +6,37 @@ import (
 )
 
 func main() {
-	os.WriteFile("hello.txt", []byte("Hello World"), fs.ModePerm)
+	if err := os.WriteFile("/data/hello.txt", []byte("Hello World"), fs.ModePerm); err != nil {
+		println(err.Error())
+		os.Exit(1)
+	}
 
-	os.Mkdir("meow", fs.ModePerm)
-	os.WriteFile("meow/meow.txt", []byte("Meow Meow"), fs.ModePerm)
+	if err := os.Mkdir("/data/meow", fs.ModePerm); err != nil {
+		println(err.Error())
+		os.Exit(1)
+	}
+	if err := os.WriteFile("/data/meow/meow.txt", []byte("Meow Meow"), fs.ModePerm); err != nil {
+		println(err.Error())
+		os.Exit(1)
+	}
 
-	os.Mkdir("meow/woof", fs.ModePerm)
-	os.WriteFile("meow/woof/woof.txt", []byte("Woof Woof"), fs.ModePerm)
+	if err := os.Mkdir("/data/meow/woof", fs.ModePerm); err != nil {
+		println(err.Error())
+		os.Exit(1)
+	}
+	if err := os.WriteFile("/data/meow/woof/woof.txt", []byte("Woof Woof"), fs.ModePerm); err != nil {
+		println(err.Error())
+		os.Exit(1)
+	}
 
-	os.Mkdir("meow/woof/moo", fs.ModePerm)
-	os.WriteFile("meow/woof/moo/moo.txt", []byte("Moo Moo"), fs.ModePerm)
+	if err := os.Mkdir("/data/meow/woof/moo", fs.ModePerm); err != nil {
+		println(err.Error())
+		os.Exit(1)
+	}
+	if err := os.WriteFile("/data/meow/woof/moo/moo.txt", []byte("Moo Moo"), fs.ModePerm); err != nil {
+		println(err.Error())
+		os.Exit(1)
+	}
+
+	os.Exit(1)
 }
