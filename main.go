051d9f072b9c3c9fba62be394caadafd7069a283
Cody Miller <hello@looskie.com>
2023-08-22 20:18:18 -0400

    create and empty dir too

--- main.go
+++ main.go
@@ -8,34 +8,31 @@ import (
 func main() {
 	if err := os.WriteFile("/data/hello.txt", []byte("Hello World"), fs.ModePerm); err != nil {
 		println(err.Error())
-		os.Exit(1)
 	}
 
 	if err := os.Mkdir("/data/meow", fs.ModePerm); err != nil {
 		println(err.Error())
-		os.Exit(1)
 	}
 	if err := os.WriteFile("/data/meow/meow.txt", []byte("Meow Meow"), fs.ModePerm); err != nil {
 		println(err.Error())
-		os.Exit(1)
 	}
 
 	if err := os.Mkdir("/data/meow/woof", fs.ModePerm); err != nil {
 		println(err.Error())
-		os.Exit(1)
 	}
 	if err := os.WriteFile("/data/meow/woof/woof.txt", []byte("Woof Woof"), fs.ModePerm); err != nil {
 		println(err.Error())
-		os.Exit(1)
 	}
 
 	if err := os.Mkdir("/data/meow/woof/moo", fs.ModePerm); err != nil {
 		println(err.Error())
-		os.Exit(1)
 	}
 	if err := os.WriteFile("/data/meow/woof/moo/moo.txt", []byte("Moo Moo"), fs.ModePerm); err != nil {
 		println(err.Error())
-		os.Exit(1)
+	}
+
+	if err := os.Mkdir("/data/meow/woof/empty", fs.ModePerm); err != nil {
+		println(err.Error())
 	}
 
 	os.Exit(1)
